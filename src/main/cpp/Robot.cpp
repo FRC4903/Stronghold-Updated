05fc23a3bd421a30ad141095d5a4bbcabebdd935
MarcoKurepa <marco.n.kurepa@gmail.com>
2022-12-05 10:39:35 -0500

    Revert "some name"
    This reverts commit 2e17d55ba5ea08d5654f22014c844cccc7f79256.

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -17,18 +17,8 @@ using namespace frc;
 using namespace std;
 using namespace rev;
 
-
-
 class Robot: public TimedRobot {
   public:
-  bool init=true;
-  bool f1=false;
-  bool t1=false;
-  bool t2=false;
-  bool f2=false;
-  bool shoot=false;
-  bool complete=false;
-
   //Joystick axes
   int joy_x2 = 4;
   int joy_y1 = 5;
@@ -105,7 +95,6 @@ class Robot: public TimedRobot {
   void AutonomousInit() override {
     gameTimer -> Start();
     gameTimer -> Reset();
-
    
     ultra ->SetAutomaticMode(true);
     SmartDashboard::PutNumber("Power",0);
@@ -114,7 +103,6 @@ class Robot: public TimedRobot {
   }
 
   void AutonomousPeriodic() override {
-    
 
     float y1 ;
     float y2 ;
@@ -148,7 +136,13 @@ class Robot: public TimedRobot {
       y2 = 0;
       }
     }else if(m_Selection == AutoOptions::Auto3){
-      
+      bool init=true;
+      bool f1=false;
+      bool t1=false;
+      bool t2=false;
+      bool f2=false;
+      bool shoot=false;
+      bool complete=false;
       double dist = ultra -> GetRange().value();
       double target = SmartDashboard::GetNumber("straight meters",0);
       double target2 = SmartDashboard::GetNumber("straight meters 2",0);
@@ -164,7 +158,6 @@ class Robot: public TimedRobot {
       }else if (dist >= 2.7 && f1){
         y1 = -1.0;
         y2 = 1.0;
-        
       }else if (dist < 2.7 && f1){
         y1 = 0;
         y2 = 0;
